7b782ef0a0c072c1619e9736d80207efaede22cd
Ronak <ronakasnani5@gmail.com>
2021-04-01 01:47:53 +0530

    recursion

--- Advanced recursion/quickSort.cpp
+++ Advanced recursion/quickSort.cpp
@@ -1,11 +1,26 @@
+/* Sort an array A using Quick Sort.
+Change in the input array itself. So no need to return or print anything.
+Input format :
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+Array elements in increasing order (separated by space)
+Constraints :
+1 <= n <= 1000
+Sample Input:
+6 
+2 6 8 5 4 3
+Sample Output:
+2 3 4 5 6 8 */
+
 #include <iostream>
 using namespace std;
 
-int partition(int *a, int s, int e)
+int partition(int *a, int st, int en)
 {
-    int pivot = a[e];
-    int i = s - 1;
-    for (int j = s; j < e; j++)
+    int pivot = a[en];
+    int i = st - 1;
+    for (int j = st; j < en; j++)
     {
         if (a[j] < pivot)
         {
@@ -15,20 +30,22 @@ int partition(int *a, int s, int e)
             a[j] = temp;
         }
     }
-    int temp = a[i + 1];
-    a[i + 1] = a[e];
-    a[e] = temp;
+    int temp = a[en];
+    a[en] = a[i + 1];
+    a[i + 1] = temp;
     return i + 1;
 }
 
-void quicksort(int *a, int s, int e)
+void quicksort(int *a, int st, int en)
 {
-    if (s < e)
+    if (st >= en)
     {
-        int pi = partition(a, s, e);
-        quicksort(a, s, pi - 1);
-        quicksort(a, pi + 1, e);
+        return;
     }
+    int p = partition(a, st, en);
+    quicksort(a, st, p - 1);
+    quicksort(a, p + 1, en);
+    return;
 }
 
 int main()
